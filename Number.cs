1477cec659148669a16fad46043ee246fbe1ce84
aonyshchuk <alinaonyshchuk7@gmail.com>
2023-09-12 16:44:01 +0300

    Add project files.

---
+++ Number.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_4
+{
+    namespace NumberGenerators
+    {
+        public class EvenNumberGenerator
+        {
+            private int currentNumber;
+
+            public EvenNumberGenerator()
+            {
+                currentNumber = 0;
+            }
+
+            public int GenerateNext()
+            {
+                currentNumber += 2;
+                return currentNumber;
+            }
+        }
+    }
+
+    namespace NumberGenerators
+    {
+        public class OddNumberGenerator
+        {
+            private int currentNumber;
+
+            public OddNumberGenerator()
+            {
+                currentNumber = 1;
+            }
+
+            public int GenerateNext()
+            {
+                currentNumber += 2;
+                return currentNumber;
+            }
+        }
+    }
+
+    namespace NumberGenerators
+    {
+        public class PrimeNumberGenerator
+        {
+            private int currentNumber;
+
+            public PrimeNumberGenerator()
+            {
+                currentNumber = 2;
+            }
+
+            public int GenerateNext()
+            {
+                while (!IsPrime(currentNumber))
+                {
+                    currentNumber++;
+                }
+
+                int primeNumber = currentNumber;
+                currentNumber++;
+                return primeNumber;
+            }
+
+            private bool IsPrime(int number)
+            {
+                if (number < 2)
+                {
+                    return false;
+                }
+
+                for (int i = 2; i <= Math.Sqrt(number); i++)
+                {
+                    if (number % i == 0)
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        }
+    }
+
+    namespace NumberGenerators
+    {
+        public class FibonacciGenerator
+        {
+            public int GenerateNext(int n)
+            {
+                if (n <= 0)
+                    throw new ArgumentException("n must be a positive integer.", nameof(n));
+
+                if (n == 1)
+                    return 0;
+
+                if (n == 2)
+                    return 1;
+
+                int previous = 0;
+                int current = 1;
+                int next = 0;
+
+                for (int i = 3; i <= n; i++)
+                {
+                    next = previous + current;
+                    previous = current;
+                    current = next;
+                }
+
+                return next;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using C_4;
+using C_4.NumberGenerators;
+using System;
+using System.Reflection.Emit;
+using System.Text;
+
+namespace C_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            EvenNumberGenerator evenGenerator = new EvenNumberGenerator();
+            OddNumberGenerator oddGenerator = new OddNumberGenerator();
+            PrimeNumberGenerator primeGenerator = new PrimeNumberGenerator();
+            FibonacciGenerator fibonacciGenerator = new FibonacciGenerator();
+
+
+            Console.WriteLine("Парні числа:");
+            for (int i = 0; i < 5; i++)
+            {
+                int evenNumber = evenGenerator.GenerateNext();
+                Console.WriteLine(evenNumber);
+            }
+
+
+            Console.WriteLine("\nНепарні числа:");
+            for (int i = 0; i < 5; i++)
+            {
+                int oddNumber = oddGenerator.GenerateNext();
+                Console.WriteLine(oddNumber);
+            }
+
+
+            Console.WriteLine("\nПрості числа:");
+            for (int i = 0; i < 5; i++)
+            {
+                int primeNumber = primeGenerator.GenerateNext();
+                Console.WriteLine(primeNumber);
+            }
+
+
+            Console.WriteLine("\nЧисла Фібоначчі:");
+            for (int i = 1; i <= 5; i++)
+            {
+                int fibonacciNumber = fibonacciGenerator.GenerateNext(i);
+                Console.WriteLine(fibonacciNumber);
+            }
+        }
+
+    }
+
+}
+
+
